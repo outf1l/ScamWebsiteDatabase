7a691c9f1f9ccb8149a7b9c9231b98545ee979a4
outf1l <outfil098@gmail.com>
2025-05-21 17:53:55 +0300

    Update scamWebsites.js

--- scamWebsites.js
+++ scamWebsites.js
@@ -10,4 +10,8 @@ var scamWebsitesRegEx = [
 	"^https?:\/\/(www\.)?plateforme-transports\.com(\/.*)?$/gm", // Accused to be phishing by PhishTank
 	"^https?:\/\/(www\.)?tartebakery\.ir(\/.*)?$/gm", // Accused to be phishing by PhishTank
 	"^https?:\/\/(www\.)?supportealarez\.online(\/.*)?$/gm", // Accused to be phishing by PhishTank
+	"^https?:\/\/(www\.)?alia33\.store(\/.*)?$/gm", // Grow a Garden scam (or at least a Roblox scam) + notifications request
+	"^https?:\/\/(www\.)?nationalconsumerscenter\.co\.uk(\/.*)?$/gm", // Survey scam (UK)
+	"^https?:\/\/(www\.)?e\.mobiapp-games\.net(\/.*)?$/gm", // Phone number scam? (Germany)
+	"^https?:\/\/(www\.)?rewardsgiantca\.com(\/.*)?$/gm", // Survey scam (Canada)
 ]
