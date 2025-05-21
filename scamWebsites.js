c8d28234db7ffed22d20db788acc588035305d51
outf1l <outfil098@gmail.com>
2025-05-21 17:19:03 +0300

    Update scamWebsites.js

--- scamWebsites.js
+++ scamWebsites.js
@@ -7,11 +7,3 @@ var scamWebsitesRegEx = [
 	"^https?:\/\/(www\.)?noticiasdeagoraaa\.fr(\/.*)?$/gm", // Accused to be phishing by PhishTank
 	"^https?:\/\/(www\.)?plateforme-transports\.com(\/.*)?$/gm", // Accused to be phishing by PhishTank
 ]
-
-// Function part
-var url = window.location.href;
-var isScam = scamWebsitesRegEx.some(pattern => new RegExp(pattern).test(url));
-
-if (isScam) {
-    alert("Scam site detected!");
-}
