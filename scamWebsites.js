a92cbe60d7d7ffdd94f94738cbc582f17567813e
outf1l <outfil098@gmail.com>
2025-05-21 17:24:07 +0300

    Update scamWebsites.js

--- scamWebsites.js
+++ scamWebsites.js
@@ -6,6 +6,6 @@ var scamWebsitesRegEx = [
 	"^https?:\/\/(www\.)?montepio24\.com(\/.*)?$/gm", // Accused to be phishing by PhishTank
 	"^https?:\/\/(www\.)?noticiasdeagoraaa\.fr(\/.*)?$/gm", // Accused to be phishing by PhishTank
 	"^https?:\/\/(www\.)?plateforme-transports\.com(\/.*)?$/gm", // Accused to be phishing by PhishTank
-	"^https?:\/\/(www\.)?tartebakery\.ir(\/.*)?$/gm, // Accused to be phishing by PhishTank
+	"^https?:\/\/(www\.)?tartebakery\.ir(\/.*)?$/gm", // Accused to be phishing by PhishTank
 	"^https?:\/\/(www\.)?supportealarez\.online(\/.*)?$/gm", // Accused to be phishing by PhishTank
 ]
