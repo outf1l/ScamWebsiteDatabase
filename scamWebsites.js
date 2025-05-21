ed52ffaca7a3e7d560e6aa757ada5174ca45e034
outf1l <outfil098@gmail.com>
2025-05-21 18:24:25 +0300

    Update scamWebsites.js

--- scamWebsites.js
+++ scamWebsites.js
@@ -10,7 +10,9 @@ var scamWebsitesRegEx = [
 	"^https?:\/\/(www\.)?plateforme-transports\.com(\/.*)?$/gm", // Accused to be phishing by PhishTank
 	"^https?:\/\/(www\.)?tartebakery\.ir(\/.*)?$/gm", // Accused to be phishing by PhishTank
 	"^https?:\/\/(www\.)?supportealarez\.online(\/.*)?$/gm", // Accused to be phishing by PhishTank
-	"^https?:\/\/(www\.)?alia33\.store(\/.*)?$/gm", // Grow a Garden scam (or at least a Roblox scam) + notifications request
+	"^https?:\/\/(www\.)?alia33\.store(\/.*)?$/gm", // Grow a Garden scam (or at least a Roblox scam or Free robux scam) + notifications request
+	"^https?:\/\/(www\.)?alia33\.store\/Roblxo(\/.*)?$/gm", // Grow a Garden scam (or at least a Roblox scam or Free robux scam) + notifications request. Roblox stage
+	"^https?:\/\/(www\.)?alia33\.store\/Roblxo\/locker\.html(\/.*)?$/gm", // Grow a Garden scam (or at least a Roblox scam or Free robux scam) + notifications request. Locker stage
 	"^https?:\/\/(www\.)?nationalconsumerscenter\.co\.uk(\/.*)?$/gm", // Survey scam (UK)
 	"^https?:\/\/(www\.)?e\.mobiapp-games\.net(\/.*)?$/gm", // Phone number scam? (Germany)
 	"^https?:\/\/(www\.)?rewardsgiantca\.com(\/.*)?$/gm", // Survey scam (Canada)
@@ -18,4 +20,8 @@ var scamWebsitesRegEx = [
 	"^https?:\/\/(www\.)?nl\.prijzen-winnaar\.com(\/.*)?$/gm", // Fake "Celebrate your mother with a gift" page (Netherlands)
 	"^https?:\/\/(www\.)?promotion\.quiztionnaire\.app(\/.*)?$/gm", // Survey scam? (Netherlands)
 	"^https?:\/\/(www\.)?app\.quiztionnaire-nl\.com(\/.*)?$/gm", // Survey scam? (Netherlands)
+	"^https?:\/\/(www\.)?ru\.ubundle\.app(\/.*)?$/gm", // Phone Number scam (Russia)
+	"^https?:\/\/(www\.)?sklinqasi\.com(\/.*)?$/gm", // Survey Scam? (Russia)
+	"^https?:\/\/(www\.)?dxv9ab0p31jil\.cloudfront\.net(\/.*)?$/gm", // Phone Number scam (Russia/USA)
+	"^https?:\/\/(www\.)?it.adessogratis.com(\/.*)?$/gm", // Survey/Credentials scam? (France)
 ]
