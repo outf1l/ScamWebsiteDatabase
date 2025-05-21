ef0b39ea3484d5feabcb850f81e069086cbab3ea
outf1l <outfil098@gmail.com>
2025-05-21 17:04:30 +0300

    Create scamWebsites.js

---
+++ scamWebsites.js
@@ -0,0 +1,9 @@
+var scamWebsitesRegEx = [
+	"^https?:\/\/(www\.)?powrnewsu\.click(\/.*)?$", // Fake news site
+	"^https?:\/\/(www\.)?instagramsecure\.com(\/.*)?$/gm", // Instagram phishing site
+	"^https?:\/\/(www\.)?idealassistenci\.com(\/.*)?$/gm", // Accused to be phishing by PhishTank
+	"^https?:\/\/(www\.)?idealconecta\.com(\/.*)?$/gm", // Accused to be phishing by PhishTank
+	"^https?:\/\/(www\.)?montepio24\.com(\/.*)?$/gm", // Accused to be phishing by PhishTank
+	"^https?:\/\/(www\.)?noticiasdeagoraaa\.fr(\/.*)?$/gm", // Accused to be phishing by PhishTank
+	"^https?:\/\/(www\.)?plateforme-transports\.com(\/.*)?$/gm", // Accused to be phishing by PhishTank
+]
