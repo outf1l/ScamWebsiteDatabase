59c5b28e2865dcaaebbbf0ba324c53a2b5148f1a
outf1l <outfil098@gmail.com>
2025-05-21 17:23:12 +0300

    Update scamWebsites.js

--- scamWebsites.js
+++ scamWebsites.js
@@ -5,5 +5,7 @@ var scamWebsitesRegEx = [
 	"^https?:\/\/(www\.)?idealconecta\.com(\/.*)?$/gm", // Accused to be phishing by PhishTank
 	"^https?:\/\/(www\.)?montepio24\.com(\/.*)?$/gm", // Accused to be phishing by PhishTank
 	"^https?:\/\/(www\.)?noticiasdeagoraaa\.fr(\/.*)?$/gm", // Accused to be phishing by PhishTank
-	"^https?:\/\/(www\.)?plateforme-transports\.com(\/.*)?$/gm", // Accused to be phishing by PhishTank,
+	"^https?:\/\/(www\.)?plateforme-transports\.com(\/.*)?$/gm", // Accused to be phishing by PhishTank
+	"^https?:\/\/(www\.)?tartebakery\.ir(\/.*)?$/gm, // Accused to be phishing by PhishTank
+	"^https?:\/\/(www\.)?supportealarez\.online(\/.*)?$/gm, // Accused to be phishing by PhishTank
 ]
